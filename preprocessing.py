5cebc5d96a9cafb82534f92571c3553545fcbc5a
Jason <jasonha444@gmail.com>
2026-06-17 19:47:13 -0700

    fixed typo in imports. 'skimpy' -> 'skimage'

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.utils import shuffle
-from skimpy import exposure
+from skimage import exposure
 
 def preprocess_data(X, y=None):
     #Convert to grayscale
@@ -17,5 +17,6 @@ def preprocess_data(X, y=None):
         #Shuffle data
         y=np.eye(43)[y]
         X, y = shuffle(X, y, random_state=42)
+
     X= X.reshape(X.shape +(1,))
     return X, y
